9dd1d3df4a7622865632fc906731ce7126ebdd58
Lucas <lucariel92@gmail.com>
2020-12-03 18:22:57 -0300

    examples of functions

--- R/exploratory.R
+++ R/exploratory.R
@@ -6,20 +6,58 @@ usethis::use_package("ggpubr")
 usethis::use_package("factoextra")
 
 
-
-
+#' get_MCA_plot
+#' Performs a multicorrelation analysis
+#' on the experimient variables to
+#' see if there is a pattern in the distribution
+#' @importFrom magrittr %>%
+#' @param experiment data.frame with "response_id" column
+#'
+#' @return ggplot2 object
+#'
+#' df = data.frame(
+#'    response_id = runif(100),
+#'    a = runif(100),
+#'    b = runif(100)
+#' )
+#' get_MCA_plot(df)
+#' @export
 get_MCA_plot<-function(experiment){
-  expm_mca = experiment %>% dplyr::select(-response_id)
+  expm_mca = experiment %>% dplyr::select(-'response_id')
   expm_mca<-tidyr::as_tibble(sapply(expm_mca  , as.factor))
   res.mca <- FactoMineR::MCA(expm_mca,graph = F)
   factoextra::fviz_screeplot(res.mca, addlabels = TRUE, ylim = c(0, 45))
 }
 
 
-
+#'ballon_plot
+#'Creates the ballon plot to
+#'visualize how each phrase of the
+#'experimient affects the answer
+#' @importFrom magrittr %>%
+#' @param a Parameter selected for the columns
+#' @param b Parameter selected for the rows
+#' @param exp Dataframe containing cols "a" and "b"
+#' @example
+#'  a = c(rep(x = 'red',round(runif(1)*100,0)),rep(x = 'blue',round(runif(1)*100,0)),rep(x = 'yellow',round(runif(1)*100,0)))
+#'  b = c(rep(x = 'shoe',round(runif(1)*100,0)),rep(x = 'hat',round(runif(1)*100,0)),rep(x = 'shirt',round(runif(1)*100,0)))
+#'  if(length(a)<length(b)){
+#'      b = b[1:length(a)]
+#'  }
+#'  else{
+#'     a = a[1:length(b)]
+#'  }
+#'
+#'  exp = data.frame(
+#'  column1 = a,
+#'  column2 = b
+#'  )
+#'  ballon_plot('column1','column2',exp)
+#'
+#' @export
 ballon_plot<-function(a,b,exp){
   tabla=prop.table(table(as.vector(unlist(exp[a])),as.vector(unlist(exp[b])))) %>% as.data.frame()
   ggpubr::ggballoonplot(tabla, fill = "value")+
-    ggplot2::scale_fill_viridis_c(option = "C")+ theme(legend.position = "none")
+    ggplot2::scale_fill_viridis_c(option = "C")+ ggplot2::theme(legend.position = "none")
 }
 
--- R/model.R
+++ R/model.R
@@ -2,24 +2,33 @@ usethis::use_package("ggplot2")
 usethis::use_package("dplyr")
 usethis::use_package("tidyr")
 usethis::use_package("magrittr")
-#usethis::use_package("rcompanion")
 usethis::use_package("broom")
 
-
+#'variable_selection_cramerV
+#'
+#'Uses Cramer's V to identify the variables
+#'more correlated with the answer, and drops
+#'those demographic variables which have no
+#'correlation between it and the answer
+#' @importFrom magrittr %>%
+#' @param survey_experiment The dataframe with survey and experiment data, must contain "response_id" and "answer" columns
+#' @param cramer_threshold  float: a threshold to consider a variable relevant or not according to Cramer's V
+#'survey_exp = data.frame(
+#'  response_id = runif(100),
+#'  answer = c(rep(1,65), rep(0,35)),
+#')
+#'survey_exp['a']=rnorm(100,3)*survey_exp$answer
+#'survey_exp['b']=(survey_exp$answer+0.1)/3
+#'survey_exp['c']=runif(100)
+#'variable_selection_cramerV(survey_exp,0.1)
+#' @export
 variable_selection_cramerV<-function(survey_experiment, cramer_threshold){
-  coenf<-survey_experiment %>% dplyr::select(-response_id)%>%
+  coenf<-survey_experiment %>% dplyr::select(-'response_id')%>%
     tidyr::gather("variable", "measure", -answer)%>%
-    group_by(variable) %>% dplyr::filter(!is.na(measure)) %>%
-    do(broom::tidy(rcompanion::cramerV(.$answer, .$measure,bias.correct = T)))
+    dplyr::group_by(variable) %>% dplyr::filter(!is.na(measure)) %>%
+    dplyr::do(broom::tidy(rcompanion::cramerV(.$answer, .$measure,bias.correct = T)))
   vars_interes<-coenf %>% dplyr::filter(variable%in%colnames(exp) | x > cramer_threshold )
   vars_interes
 }
 
-test_train_split<-function(p,dt_select){
-  smp_size <- floor(p * nrow(dt_select))
-  train_idx <- sample(seq_len(nrow(dt_select)), size = smp_size)
-  train <- dt_select[train_idx, ]
-  test <- dt_select[-train_idx, ]
-  list(train,test)
-}
 
